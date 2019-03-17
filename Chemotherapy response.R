3acceac758d3e6501d97730902ec4b7b784d197c
SoMuchForSubtilty <rochelancaster@gmail.com>
2019-03-17 13:22:37 +0000

    Found some of the problems

--- Chemotherapy response.R
+++ Chemotherapy response.R
@@ -50,7 +50,8 @@ data.chemo.pro.cor <- alt.data11[,-1]
 correlationMatrix.pro <- cor(progress..yes.no., data.chemo.pro.cor)
 print(correlationMatrix.pro)
 #May change cutoff if results are poor
-highlyCorrelated.pro <- findCorrelation(correlationMatrix.pro, cutoff=0.5)
+#Have some missing values
+highlyCorrelated.pro <- findCorrelation(correlationMatrix.pro, cutoff=0.75)
 print(highlyCorrelated.pro)
 
 #3 Learning Vector Quantization
@@ -88,7 +89,7 @@ model.chemo.gbm.pro <- train(progress..yes.no. ~ A_EGFR + A_ATM + A_FLCN + A_CDK
                       preProcess = c("center", "scale"),
                       tuneLength = 10,
                       na.action = na.pass)
-#This function cannot be found
+
 importance.chemo.gbm.pro <- varImp(model.chemo.gbm.pro, scale = FALSE)
 print(importance.chemo.gbm.pro)
 plot(importance.chemo.gbm.pro)
@@ -112,6 +113,7 @@ set.seed(126)
 data.chemo.PFS.cor <- alt.data11.PFS[,-1]
 correlationMatrix.PFS <- cor(progress..yes.no., data.chemo.PFS.cor)
 print(correlationMatrix.PFS)
+#Some missing values
 #May change cutoff if results are poor
 highlyCorrelated.PFS <- findCorrelation(correlationMatrix.PFS, cutoff=0.5)
 print(highlyCorrelated.PFS)
@@ -119,6 +121,7 @@ print(highlyCorrelated.PFS)
 #3 Learning Vector Quantization
 test.chemo.lvq.PFS <- trainControl(method = "repeatedcv", number = 10, repeats = 5)
 set.seed(86)
+#Wrong model type for regression
 model.chemo.lvq.PFS <- train(progress..yes.no. ~ A_EGFR + A_ATM + A_FLCN + A_CDK4 + A_TERT + A_PBRM1 + A_MAP3K1 + A_ATRX + A_PPP2R1A + A_SMARCB1 + A_ABL1 + A_PALB2 + A_MYC + A_MED12 + A_DAXX + A_CSFR1 + A_U2AF1 + A_CREBBP + A_FBXO11 + A_CDH1 + A_ZNFR3 + A_ARID1A + A_GRIN2A + A_BCL6 + A_TSC2 + A_BRCA1 + A_CDK12 + A_PTCH1 + A_TP53 + A_EZH2 + A_MSH2 + A_AR + A_GNA11 + A_SMAD4 + A_BRCA2 + A_RB1 + A_CTNNB1 + A_KIT + A_SETD2 + A_KDR + A_JAK1 + A_WT1 + A_SLC7A8 + A_KMT2D + A_NF1 + A_NOTCH1 + A_KDM6A + A_CIC + A_PRDM1 + A_JAK2 + A_RET + A_BCOR + A_ESR1 + A_APC + A_FBXW7 + A_GNAS + A_MEN1 + A_DNMT3A + A_ZNRF3 + A_FANCE + A_TSC1 + A_SMARC4A + A_MSH6 + A_EP300 + A_PRKAR1A + A_BUB1B + A_none + A_MLH1 + A_H3F3A + A_SMO + A_BRAF + B_APC + B_SMARC4 + B_BAP1 + B_NPM1 + B_FGFR3 + B_CIC + B_GNA11 + B_IL7R + B_GNAS + B_KMT2D + B_MAP3K1 + B_ZRSR2 + B_DDB2 + B_MDM2 + B_MEN1 + B_none + B_PIK3R1 + B_ARID2 + B_ABL1 + B_NOTCH1 + B_TERT + B_CDK4 + B_CSF1R + B_VHL + B_MYC + B_TSC1 + B_PRKAR1A + B_DDR2 + B_SLC7A8 + B_STK11 + B_IL6ST + B_U2AF1 + B_ASXL1 + C_CARD11 + C_SDHB + C_TNRFSF14 + C_CYLD + C_SLC7A8 + C_PHF6 + C_FGFR2 + C_SPOP + C_MLH1 + C_none + C_BRIP1 + C_MSH2 + C_CD79 + C_SMO + C_GNAQ + C_ECT2L + C_CDC73 + C_PIK3R1 + C_TNFSRF14 + C_BRAF + C_RB1 + C_SMAD4 + C_MAP2K4 + C_SMARCB1 + C_DNMT3A + C_BRCA2 + C_JAK1 + C_EZH2 + C_FAS + C_NF1 + C_CHEK2 + C_PALB2 + C_CSF1R + C_TNFSRSF14 + C_CDK12 + C_FLCN + C_FLNC + C_MAP4K3 + C_EPCAM + C_TNFRSF14 + C_VHL, 
                          data = alt.data11.PFS, method = "lvq",
                          trControl = test.chemo.lvq.PFS,
@@ -132,6 +135,7 @@ plot(importance.chemo,lvq.PFS)
 #4 Support Vector Machine
 test.chemo.svm.PFS <- trainControl(method = "repeatedcv", number = 10, repeats = 5)
 set.seed(86)
+#Undefined columns selected
 model.chemo.svm.PFS <- train(progress..yes.no. ~ A_EGFR + A_ATM + A_FLCN + A_CDK4 + A_TERT + A_PBRM1 + A_MAP3K1 + A_ATRX + A_PPP2R1A + A_SMARCB1 + A_ABL1 + A_PALB2 + A_MYC + A_MED12 + A_DAXX + A_CSFR1 + A_U2AF1 + A_CREBBP + A_FBXO11 + A_CDH1 + A_ZNFR3 + A_ARID1A + A_GRIN2A + A_BCL6 + A_TSC2 + A_BRCA1 + A_CDK12 + A_PTCH1 + A_TP53 + A_EZH2 + A_MSH2 + A_AR + A_GNA11 + A_SMAD4 + A_BRCA2 + A_RB1 + A_CTNNB1 + A_KIT + A_SETD2 + A_KDR + A_JAK1 + A_WT1 + A_SLC7A8 + A_KMT2D + A_NF1 + A_NOTCH1 + A_KDM6A + A_CIC + A_PRDM1 + A_JAK2 + A_RET + A_BCOR + A_ESR1 + A_APC + A_FBXW7 + A_GNAS + A_MEN1 + A_DNMT3A + A_ZNRF3 + A_FANCE + A_TSC1 + A_SMARC4A + A_MSH6 + A_EP300 + A_PRKAR1A + A_BUB1B + A_none + A_MLH1 + A_H3F3A + A_SMO + A_BRAF + B_APC + B_SMARC4 + B_BAP1 + B_NPM1 + B_FGFR3 + B_CIC + B_GNA11 + B_IL7R + B_GNAS + B_KMT2D + B_MAP3K1 + B_ZRSR2 + B_DDB2 + B_MDM2 + B_MEN1 + B_none + B_PIK3R1 + B_ARID2 + B_ABL1 + B_NOTCH1 + B_TERT + B_CDK4 + B_CSF1R + B_VHL + B_MYC + B_TSC1 + B_PRKAR1A + B_DDR2 + B_SLC7A8 + B_STK11 + B_IL6ST + B_U2AF1 + B_ASXL1 + C_CARD11 + C_SDHB + C_TNRFSF14 + C_CYLD + C_SLC7A8 + C_PHF6 + C_FGFR2 + C_SPOP + C_MLH1 + C_none + C_BRIP1 + C_MSH2 + C_CD79 + C_SMO + C_GNAQ + C_ECT2L + C_CDC73 + C_PIK3R1 + C_TNFSRF14 + C_BRAF + C_RB1 + C_SMAD4 + C_MAP2K4 + C_SMARCB1 + C_DNMT3A + C_BRCA2 + C_JAK1 + C_EZH2 + C_FAS + C_NF1 + C_CHEK2 + C_PALB2 + C_CSF1R + C_TNFSRSF14 + C_CDK12 + C_FLCN + C_FLNC + C_MAP4K3 + C_EPCAM + C_TNFRSF14 + C_VHL, 
                              data = alt.data11.PFS, method = "svmRadial",
                              trControl = test.chemo.svm.PFS,
@@ -163,3 +167,4 @@ results.chemo.rf.PFS <- rfe(alt.data11.PFS[,2:146], alt.data11.PFS[,1], sizes = 
 print(results.chemo.rf.PFS)
 predictors(results.chemo.rf.PFS)
 plot(results.chemo.rf.PFS, type = c("g", "o"))
+
