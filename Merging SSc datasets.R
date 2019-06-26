d503d76c0128790979122eb0d6fc03b172f7f617
Oisin Roche-Lancaster <rochelancaster@gmail.com>
2019-06-26 18:32:27 +0200

    Adding some subsettings

--- Merging SSc datasets.R
+++ Merging SSc datasets.R
@@ -1,5 +1,8 @@
 library(GEOquery)
 library(VennDiagram)
+library(dplyr)
+
+
 
 GSE106358 <- getGEO("GSE106358") #Txt type file
 GSE94340 <- getGEO("GSE94340") #CEL type file
@@ -43,7 +46,58 @@ select1b <- c("title", "geo_accession", "status", "submission_date", "last_updat
             "platform_id", "contact_name", "contact_department", "contact_institute", 
             "contact_address", "contact_city", "contact_zip/postal_code", "contact_country", 
             "supplementary_file", "data_row_count")
-selected1b <- eset1b[select1b]
+
+setdiff(colnames(pData(eset1b)), select1b)
+
+eset1b$growth_protocol_ch1 <- NULL
+eset1b$source_name_ch2 <- NULL
+eset1b$organism_ch2 <- NULL
+eset1b$characteristics_ch2 <- NULL
+eset1b$characteristics_ch2.1<- NULL
+eset1b$characteristics_ch2.2 <- NULL
+eset1b$characteristics_ch2.3 <- NULL
+eset1b$characteristics_ch2.4 <- NULL
+eset1b$characteristics_ch2.5 <- NULL
+eset1b$characteristics_ch2.6 <- NULL
+eset1b$characteristics_ch2.7 <- NULL
+eset1b$treatment_protocol_ch2 <- NULL
+eset1b$growth_protocol_ch2 <- NULL
+eset1b$molecule_ch2 <- NULL
+eset1b$extract_protocol_ch2 <- NULL
+eset1b$label_ch2 <- NULL
+eset1b$label_protocol_ch2 <- NULL
+eset1b$taxid_ch2 <- NULL
+eset1b$description <- NULL
+eset1b$description.1 <- NULL
+eset1b$contact_email <- NULL
+eset1b$age:ch2 <- NULL
+eset1b$'disease state:ch2' <- NULL
+eset1b$gender:ch2 <- NULL
+eset1b$mrss:ch2 <- NULL
+eset1b$'sample type:ch1' <- NULL
+eset1b$subject:ch2 <- NULL
+eset1b$`time point:ch2` <- NULL
+eset1b$tissue:ch2 <- NULL
+eset1b$treatment:ch2 <- NULL
+
+
+subjectseset1b <- list("GSM2836412", "GSM2836413", "GSM2836416",
+                      "GSM2836418", "GSM2836419", "GSM2836420",
+                      "GSM2836421", "GSM2836423", "GSM2836426",
+                      "GSM2836428", "GSM2836430", "GSM2836431",
+                      "GSM2836432", "GSM2836434", "GSM2836436",
+                      "GSM2836438", "GSM2836440", "GSM2836442",
+                      "GSM2836444", "GSM2836446", "GSM2836448",
+                      "GSM2836451", "GSM2836452", "GSM2836454",
+                      "GSM2836456", "GSM2836457", "GSM2836459",
+                      "GSM2836460", "GSM2836462", "GSM2836464",
+                      "GSM2836466", "GSM2836468", "GSM2836470",
+                      "GSM2836471", "GSM2836473", "GSM2836475",
+                      "GSM2836476", "GSM2836478", "GSM2836479")
+
+#This command should work to subset for specific samples (e.g GSM2836412)
+eset1btest = eset1b[, colnames(eset1b) %in% subjectseset1b]
+
 
 #eset2b
 select2b <- c("title", "geo_accession", "status", "submission_date", "last_update_date", "type", 
@@ -53,7 +107,13 @@ select2b <- c("title", "geo_accession", "status", "submission_date", "last_updat
               "platform_id", "contact_name", "contact_department", "contact_institute", 
               "contact_address", "contact_city", "contact_zip/postal_code", "contact_country", 
               "supplementary_file", "data_row_count")
-selected2b <- eset2b[select2b]
+
+setdiff(colnames(pData(eset2b)), select2b)
+
+eset2b$characteristics_ch1.1 <- NULL
+eset2b$contact_state <- NULL
+eset2b$timepoint:ch1 <- NULL
+eset2b$treatment:ch1 <- NULL
 
 #eset3b
 select3b <- c("title", "geo_accession", "status", "submission_date", "last_update_date", "type", 
@@ -63,4 +123,36 @@ select3b <- c("title", "geo_accession", "status", "submission_date", "last_updat
               "platform_id", "contact_name", "contact_department", "contact_institute", 
               "contact_address", "contact_city", "contact_zip/postal_code", "contact_country", 
               "supplementary_file", "data_row_count")
-selected3b <- eset3b[select3b]
+
+setdiff(colnames(pData(eset3b)), select3b)
+
+eset3b$growth_protocol_ch1 <- NULL   
+eset3b$source_name_ch2 <- NULL
+eset3b$organism_ch2 <- NULL
+eset3b$characteristics_ch2 <- NULL
+eset3b$treatment_protocol_ch2 <- NULL
+eset3b$growth_protocol_ch2 <- NULL
+eset3b$molecule_ch2 <- NULL
+eset3b$extract_protocol_ch2 <- NULL
+eset3b$label_ch2 <- NULL
+eset3b$label_protocol_ch2 <- NULL
+eset3b$taxid_ch2 <- NULL
+eset3b$description <- NULL
+eset3b$contact_email <- NULL
+eset3b$contact_laboratory <- NULL
+eset3b$contact_state <- NULL
+eset3b$relation <- NULL
+eset3b$relation.1 <- NULL
+
+##### 3. Checking normalization #####
+
+boxplot(exprs())
+boxplot(exprs())
+boxplot(exprs())
+
+
+#overview of how many tumor and cancer samples are in esets
+table(pData(eset1b)[,"Tumor"])
+table(pData(eset2b)[,"Tumor"])
+table(pData(eset3b)[,"Tumor"])
+
